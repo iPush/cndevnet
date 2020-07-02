0d06fb4c856ad3318735a87a18c572cef9b0f894
Le Wang <thisiswangle@gmail.com>
2020-07-03 00:00:55 +0800

    update config file path

--- update-gfwlist.sh
+++ update-gfwlist.sh
@@ -1,5 +1,5 @@
-curl -skLo /opt/gost/gfwlist.conf https://raw.githubusercontent.com/hq450/fancyss/master/rules/gfwlist.conf
-sed -i 's/7913/15353/g' /opt/gost/gfwlist.conf
+curl -skLo /opt/qiangguo/gfwlist.conf https://raw.githubusercontent.com/hq450/fancyss/master/rules/gfwlist.conf
+sed -i 's/7913/15353/g' /opt/qiangguo/gfwlist.conf
 rm -f /etc/dnsmasq.d/gfwlist.conf | true
-ln -s /opt/gost/gfwlist.conf /etc/dnsmasq.d/gfwlist.conf
+ln -s /opt/qiangguo/gfwlist.conf /etc/dnsmasq.d/gfwlist.conf
 systemctl restart dnsmasq
