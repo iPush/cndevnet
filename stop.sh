5b7a63a4c5bb65de19d454ff191c0d8bac279319
Le Wang <lewang.dev@gmail.com>
2022-05-17 08:53:47 +0800

    add support udp proxy

--- stop.sh
+++ stop.sh
@@ -1,7 +1,17 @@
 #!/bin/bash -
+iptables -t nat -D OUTPUT -p tcp -j CNDEVNET
 iptables -t nat -F CNDEVNET 
 iptables -t nat -X CNDEVNET 
-iptables -t nat -D OUTPUT -p tcp -j CNDEVNET
+
+iptables -t mangle -D PREROUTING -j CNDEVNET
+iptables -t mangle -D OUTPUT -j CNDEVNET_LOCAL
+iptables -t mangle -F CNDEVNET
+iptables -t mangle -X CNDEVNET
+iptables -t mangle -F CNDEVNET_LOCAL
+iptables -t mangle -N CNDEVNET_LOCAL
+
+ip rule del fwmark 1 lookup 100
+ip route del local default dev lo table 100
 
 ipset destroy chnroute | true
 ipset destroy cfroute | true
