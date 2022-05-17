6c675d46ce05cc5c86d50dee6858e47627fccfe6
Le Wang <lewang.dev@gmail.com>
2022-05-17 21:46:10 +0800

    add main script

--- docker-gost.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -
-
-docker run -d \
-	--restart unless-stopped \
-	--name cndevnet-gost \
-	--network host \
-	-v ${PWD}/gost:/gost \
-	ginuerzh/gost -C /gost/config.json
--- docker-smartdns.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -
-
-docker run -d \
-	-p 53:53/udp \
-	--restart=always \
-	--name cndevnet-smartdns \
-	-v ${PWD}/smartdns:/smartdns \
-	ghostry/smartdns
